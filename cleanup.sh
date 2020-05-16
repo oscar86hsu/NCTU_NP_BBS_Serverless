ef8f4f77836313c0b553457b57acfc9a6ce2d9f5
oscar86hsu <oscar86hsu@gmail.com>
2020-05-16 17:13:27 +0800

    Update cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -7,10 +7,10 @@ while read uname; do
   aws cognito-idp admin-delete-user --user-pool-id $COGNITO_USER_POOL_ID --username $uname;
 done
 
-# aws s3api list-buckets --query 'Buckets[?starts_with(Name, `oscarhsu-nctu-bbs-`) == `true`].Name' | jq -r '.[]' |
-# while read bucket; do
-#   aws s3 rb s3://$bucket --force
-# done
+aws s3api list-buckets --query 'Buckets[?starts_with(Name, `oscarhsu-nctu-bbs-`) == `true`].Name' | jq -r '.[]' |
+while read bucket; do
+  aws s3 rb s3://$bucket --force
+done
 
 # aws dynamodb list-tables | jq -r '.TableNames | .[] | select(startswith("oscarhsu-nctu-bbs"))' |
 # while read table; do
