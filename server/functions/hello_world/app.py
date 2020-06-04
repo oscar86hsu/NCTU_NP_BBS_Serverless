4675278cb5f74ba1d4e463d316fec72ce57af783
oscar86hsu <oscar86hsu@gmail.com>
2020-06-04 13:37:31 +0000

    Modify environment variables

--- server/functions/hello_world/app.py
+++ server/functions/hello_world/app.py
@@ -1,22 +1,12 @@
 import json
 import boto3
-
-cloudformation = boto3.client('cloudformation')
-
-def get_outputs():
-    response = cloudformation.describe_stacks(
-        StackName='NCTU-BBS'
-    )
-    outputs = {}
-    for item in response['Stacks'][0]['Outputs']:
-        outputs[item['OutputKey']] = item['OutputValue']
-    return outputs
+import os
 
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "********************************\n** Welcome to the BBS server. **\n********************************",
-            "cognito_client_id": get_outputs()['UserPoolClientId']
+            "cognito_client_id": os.environ['USER_POOL_CLIENT_ID']
         }),
     }
