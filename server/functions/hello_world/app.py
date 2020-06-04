d45de3080de1c5b48900fb2558cfa5af15c6ba9e
oscar86hsu <oscar86hsu@gmail.com>
2020-06-04 14:29:38 +0000

    Add custom domain name to websocket

--- server/functions/hello_world/app.py
+++ server/functions/hello_world/app.py
@@ -7,6 +7,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "********************************\n** Welcome to the BBS server. **\n********************************",
-            "cognito_client_id": os.environ['USER_POOL_CLIENT_ID']
+            "cognito_client_id": os.environ['USER_POOL_CLIENT_ID'],
+            "websocket_url": os.environ['WEBSOCKET_URL']
         }),
     }
